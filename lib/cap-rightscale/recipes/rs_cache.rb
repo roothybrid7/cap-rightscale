3b9e3bfa31a412fc35362695742fd42f1ba81728
Satoshi Ohki <roothybrid7@gmail.com>
2011-02-02 01:48:46 +0900

    modified recipe

--- lib/cap-rightscale/recipes/rs_cache.rb
+++ lib/cap-rightscale/recipes/rs_cache.rb
@@ -1,13 +1,13 @@
-desc "Alias cache:clear"
-task :cc do
-  rs_cache.clear
-end
-
 desc "Clear rightscale's server list cache"
 namespace :rs_cache do
   task :clear do
     puts "Clear cache all"
     pp Dir.glob("#{Dir.tmpdir}/cap-rightscale-*/*")
     FileUtils.rm(Dir.glob("#{Dir.tmpdir}/cap-rightscale-*/*"), {:force => true})
+  end
+
+  desc "Alias cache:clear"
+  task :cc do
+    clear
   end
 end
