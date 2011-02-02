7a1da8ebfbf8a7bc095d210fe760f86aafc24bc7
Satoshi Ohki <roothybrid7@gmail.com>
2011-02-02 12:27:20 +0900

    add make cache tmp direcotry utils

---
+++ lib/cap-rightscale/utils/rs_utils.rb
@@ -0,0 +1,13 @@
+class RSUtils
+  def self.mk_rs_cache_dir(prefix)
+    tmpdir = Dir.tmpdir
+    _prefix = prefix || "cap-rightscale"
+    begin
+      path = "#{tmpdir}/#{_prefix}-#{ENV['USER']}-#{rand(0x100000000).to_s(36)}"
+      Dir.mkdir(path, 0700)
+    rescue Errno::EEXIST
+      logger.warn(e)
+      exit(1)
+    end
+  end
+end
